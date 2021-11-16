21556f8a719c355e406e19a25a71616f1e74c6c3
unknown <pengzilin@tenda.cn>
2021-11-16 16:15:36 +0800

    检查文件超时

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 const http = require("http");
-const fs = require("fs");
+const fs = require("fs-extra");
 const path = require("path");
 const multiparty = require("multiparty");
 
 const server = http.createServer();
 
 const pathText = path.resolve(__dirname, "./text"); //获取创建的文件夹绝对路径
+const filePath = path.resolve(__dirname, "./file"); //获取创建的文件绝对路径
+const SAVEFILETIME = 1 * 60 * 1000; //服务器文件保存时间
 
 server.on("request", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
   if (req.url == "/merge") {
     //切片传输完成
-    req.on("data", data => {
+    req.on("data", async data => {
       data = JSON.parse(data);
-      sortChunk(data); //排序合并切片
+      await sortChunk(data); //排序合并切片
 
       console.log("file is upload success");
 
       res.statusCode = 200;
       res.end("file is upload success");
     });
-  } else {
+  } else if (req.url == "/") {
     const form = new multiparty.Form();
-    form.parse(req, (err, fields, file) => {
+    form.parse(req, async (err, fields, file) => {
       const { hash, filename } = fields;
 
       if (!fs.existsSync("text")) {
@@ -31,17 +33,7 @@ server.on("request", (req, res) => {
         fs.mkdirSync("text");
       }
 
-      const pathText = path.resolve(__dirname, "./text"); //获取创建的文件夹绝对路径
-
-      const fileReadStream = fs.createReadStream(file.chunk[0].path); //创建切片文件的可读流
-
-      const chunkPath = pathText + "\\" + hash[0];
-
-      // fs.writeFileSync(chunkPath, "a"); //为每个切片创建可写空文件
-
-      const fileWriteStram = fs.createWriteStream(chunkPath); //创建文件的可写流
-
-      fileReadStream.pipe(fileWriteStram); //将切片文件通过管道写入到空文件中
+      await fs.move(file.chunk[0].path, `${pathText}/${hash}`);
 
       console.log(hash[0] + " upload is success");
 
@@ -63,17 +55,16 @@ async function sortChunk(data) {
     //如果不存在文件夹则创建文件夹
     fs.mkdirSync("file");
   }
-  const filePath = path.resolve(__dirname, "./file") + "\\" + filename;
-  // fs.writeFileSync(filePath);
+  const filePath = path.resolve(__dirname, "./file", `./${filename}`);
 
   await mergeFileChunk(fileChunkList, filePath, size);
 }
 
 async function mergeFileChunk(fileChunkList, filePath, size) {
   await Promise.all(
-    fileChunkList.map((chunkPath, index) => 
+    fileChunkList.map((chunkPath, index) =>
       pipeStream(
-        pathText + "\\" + chunkPath,
+        path.resolve(pathText, `./${chunkPath}`),
         // 指定位置创建可写流
         fs.createWriteStream(filePath, {
           start: index * size,
@@ -82,9 +73,9 @@ async function mergeFileChunk(fileChunkList, filePath, size) {
       )
     )
   );
-  console.log(fileChunkList);
   fs.rmdirSync(pathText); // 合并后删除保存切片的目录
 }
+
 const pipeStream = (path, writeStream) =>
   new Promise(resolve => {
     const readStream = fs.createReadStream(path);
@@ -98,3 +89,38 @@ const pipeStream = (path, writeStream) =>
 server.listen("3000", () => {
   console.log("正在监听3000端口");
 });
+
+//初始化
+function init() {
+  checkFileTime();
+  setInterval(checkFileTime, 1000);
+}
+//检查文件创建时间是否超时 超时则删除
+function checkFileTime() {
+  if (fs.existsSync("file")) {
+    let birthTime = "",
+      dir = "";
+    fs.readdir("./file", (err, files) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      files.forEach(filename => {
+        dir = path.resolve(filePath, filename);
+        fs.stat(dir, async (err, stats) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          birthTime = stats.birthtime;
+          if (Date.parse(new Date()) - Date.parse(birthTime) > SAVEFILETIME) {
+            await fs.unlinkSync(dir);
+            console.log(filename + " is delete");
+          }
+        });
+      });
+    });
+  }
+}
+init();
