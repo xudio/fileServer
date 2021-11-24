f229126f541c38f43257a2ee2d26e3f0fcf65e14
cynthia <64405788+xudio@users.noreply.github.com>
2021-11-24 11:54:09 +0800

    Delete auto.js

--- auto.js
+++
@@ -1,8 +0,0 @@
-const process = require("child_process");
-const ChildProcess = process.fork("./index.js");
-
-ChildProcess.on("edit", err => {
-  if (err !== 0) {
-    process.fork("./auto.js");
-  }
-});
